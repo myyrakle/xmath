1bae9b00cfc5a98338d4ac9a77971147e386ccf1
myyrakle <sssang97@naver.com>
2020-04-26 15:15:20 +0900

    Update prime_number.h

--- include/prime_number.h
+++ include/prime_number.h
@@ -1,7 +1,30 @@
 #include <vector>
+#include <cmath>
 
 namespace xmath
 {
-    bool is_prime_number(int n);
+    template <class Integer>
+    bool is_prime_number(Integer n)
+    {
+      if(n<=1)
+        return false;
+    
+      if(n%2==0)
+        return false;
+  
+      Integer divisor = 2;
+  
+      Integer root = (Integer)sqrt(n);
+      while(divisor <= root)
+      {
+        if(n%divisor == 0)
+          return false;
+      
+        divisor++;
+      }
+  
+      return true;
+    }
+
     std::vector<int> compute_prime_numbers_until(int n);
 }
