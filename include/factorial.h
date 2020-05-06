9d7d5202e9d511e95124c73e8ab3997d04544ac4
myyrakle <sssang97@naver.com>
2020-05-07 00:51:39 +0900

    Update factorial.h

--- include/factorial.h
+++ include/factorial.h
@@ -1,18 +1,19 @@
 #ifndef XMATH_FACTORIAL
 #define XMATH_FACTORIAL
 
-#include <cstdint>
 #include <exception>
 
 namespace xmath
 {
-    template <class IntegerType>
-    std::uint64_t factorial(IntegerType n)
+    template <class Integer>
+    Integer factorial(const Integer& n)
     {
       if(n<0)
+      {  
         return -1;
+      }
   
-      std::uint64_t sum = 1;
+      Integer sum = 1;
   
       while(n!=0)
       {
