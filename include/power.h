2c974bfcea19e1c9a9ab7703c4d7840529474b47
myyrakle <sssang97@naver.com>
2020-05-06 22:41:36 +0900

    Create power.h

---
+++ include/power.h
@@ -0,0 +1,34 @@
+#ifndef XMATH_POWER
+#define XMATH_POWER
+
+#include <cmath>
+#include <type_traits>
+
+namespace xmath
+{
+  template <class Integer>
+  Integer power_int(const Integer& x, const Integer& y)
+  {
+    Integer sum = x;
+    for(Integer count = y; count>1 ;count-=1)
+    {
+      sum*=x;
+    }
+  
+    return sum;
+  }
+
+  template <class Float>
+  Float power_float(const Float& x, const Float& y)
+  {
+    if(std::is_arithmetic<Float>::value)
+      return std::pow(x, y);
+    
+    Float sum = x;
+    //... ToDo implement
+  
+    return sum;
+  }
+}
+
+#endif
