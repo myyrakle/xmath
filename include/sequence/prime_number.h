3047dd8bb78b2b354350b455127dc088e90b9b10
myyrakle <sssang97@naver.com>
2020-08-02 20:24:07 +0900

    update fibonacci, prime

--- include/sequence/prime_number.h
+++ include/sequence/prime_number.h
@@ -6,30 +6,31 @@
 
 namespace xmath
 {
-    template <class Integer>
-    bool is_prime_number(Integer n)
+  template <class Integer>
+  bool is_prime_number(Integer n)
+  {
+    if (n <= 1)
+      return false;
+
+    if (n % 2 == 0)
+      return false;
+
+    Integer divisor = 2;
+
+    Integer root = (Integer)sqrt(n);
+    while (divisor <= root)
     {
-      if(n<=1)
+      if (n % divisor == 0)
         return false;
-    
-      if(n%2==0)
-        return false;
-  
-      Integer divisor = 2;
-  
-      Integer root = (Integer)sqrt(n);
-      while(divisor <= root)
-      {
-        if(n%divisor == 0)
-          return false;
-      
-        divisor++;
-      }
-  
-      return true;
+
+      divisor++;
     }
 
-    std::vector<int> compute_prime_numbers_until(int n);
-}
+    return true;
+  }
+
+  template <class Integer>
+  std::vector<Integer> prime_numbers(const Integer &first, const Integer &last);
+} // namespace xmath
 
 #endif
